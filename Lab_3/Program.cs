8a9b9d3bcba9dfd036507e0d229a07ccd2192a9f
Vlad <zorya.vladison@gmail.com>
2020-10-15 12:49:26 +0300

    He created a class TCircle, created fields and methods for it.

---
+++ Lab_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCircle circle = new TCircle(5);
+            TCircle circle2 = new TCircle(6);
+            Console.WriteLine(circle.GetRadius);
+            Console.WriteLine(circle.GetSquare());
+            Console.WriteLine(circle.GetSector(40));
+            Console.WriteLine(circle.GetCircuit());
+            Console.WriteLine(circle.ToString());
+            Console.WriteLine(circle + circle2);
+            Console.WriteLine(circle - circle2);
+            Console.WriteLine(5 * circle);
+            Console.WriteLine(circle * 5);
+        }
+    }
+}
---
+++ Lab_3/TCircle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Lab_3
+{
+    class TCircle
+    {
+        private double radius;
+
+        public TCircle()
+        {
+            this.radius = 5;
+        }
+
+        public TCircle(double radius) 
+        {
+            this.radius = radius;
+        }
+
+        public TCircle(TCircle circle) 
+        { 
+            this.radius = circle.radius;
+        } 
+
+        public double GetRadius
+        {
+            get { return radius; }
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException(nameof(value));
+                else this.radius = value;
+            }
+        }
+
+        public double GetSector(double angle)
+        {
+            return Math.PI * Math.Pow(this.radius, 2) * (angle / 360);
+        }
+
+        public double GetCircuit()
+        {
+            return 2 * Math.PI * this.radius;
+        }
+
+        public double GetSquare() 
+        {
+            return Math.PI * Math.Pow(this.radius, 2);
+        }
+
+        public static TCircle operator +(TCircle circle1, TCircle circle2) 
+        {
+            return new TCircle(circle1.radius + circle2.radius);
+        }
+
+        public static TCircle operator -(TCircle circle1, TCircle circle2) 
+        {
+            return new TCircle(Math.Abs(circle1.radius - circle2.radius));
+        }
+
+        public static TCircle operator *(TCircle circle1, double number)
+        {
+            return new TCircle(circle1.radius * number);
+        }
+            
+        public static TCircle operator *(double number, TCircle circle1)
+        {
+            return new TCircle(circle1.radius * number);
+        }
+            
+        public override string ToString()
+        {
+            return "Radius:" + radius;
+        }
+    }
+}
