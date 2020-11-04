878f5be21f41cdc53ade570091c0d7c877d0f39c
Vladyslav Zoria <zorya.vladison@gmail.com>
2020-11-04 13:38:50 +0200

    Created struct MyTime with method toString and constructor with parameters

---
+++ Lab_2/MyTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2
+{
+    struct MyTime
+    {
+        public int hour, minute, second;
+
+        public MyTime(int h, int m, int s) 
+        {
+            if(h <= 23 && m <= 59 && s <= 59) 
+            {
+                hour = h;
+                minute = m;
+                second = s;
+            }
+            else throw new Exception("Illegal time!");
+        }
+
+        public override string ToString()
+        {
+            return "T: {0:T}" + new MyTime(hour, minute, second);
+        }
+    }
+}
