59587e407391c8119d00ee9fc548f829368acab6
Vladyslav Zoria <zorya.vladison@gmail.com>
2020-12-27 23:34:03 +0200

    Added lab_4(Task1 and Task2)

---
+++ Lab_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1 task1 = new Task1();
+            task1.Method();
+            Console.ReadKey();
+            /*Task2 task2 = new Task2();
+            task2.method();*/
+        }
+    }
+}
---
+++ Lab_4/Task1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_4
+{
+    public class Task1
+    {
+        private static readonly string DefaultPathDir = $@"{Directory.GetCurrentDirectory()}\files\";
+        private int sum;
+        private int counter;
+
+        public Task1()
+        {
+            sum = 0;
+            counter = 0;
+        }
+
+        public void Method()
+        {
+            try
+            {
+                isCanOpenResultingFiles();
+                for (int i = 10; i < 30; i++)
+                {
+                    try
+                    {
+                        using (StreamReader reader = new StreamReader($"{DefaultPathDir}{i}.txt"))
+                        {
+                            int firstNumber = int.Parse(reader.ReadLine());
+                            int secondNumber = int.Parse(reader.ReadLine());
+                            sum += firstNumber * secondNumber;
+                            counter++;
+                        }
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        appendText(i, "no_file.txt", "not found");
+                    }
+                    catch (FormatException)
+                    {
+                        appendText(i, "bad_data.txt", "have not number");
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        appendText(i, "bad_data.txt", "have not number");
+                    }
+                    catch (OverflowException)
+                    {
+                        appendText(i, "overflow.txt", "so long number");
+                    }
+                }
+                Console.WriteLine(sum / counter);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Avg = 0");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+
+        private void isCanOpenResultingFiles()
+        {
+            try
+            {
+                using (StreamWriter unused = File.CreateText(DefaultPathDir + "no_file.txt"))
+                {
+                }
+
+                using (StreamWriter unused = File.CreateText(DefaultPathDir + "bad_data.txt"))
+                {
+                }
+
+                using (StreamWriter unused = File.CreateText(DefaultPathDir + "overflow.txt"))
+                {
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                throw new Exception("Can't create file");
+            }
+        }
+
+        private void appendText(int numFile, string resultingFile, string text)
+        {
+            using (FileStream file = new FileStream(DefaultPathDir + resultingFile, FileMode.Append))
+            {
+                using (StreamWriter stream = new StreamWriter(file))
+                {
+                    stream.WriteLine($"File {numFile}.txt - {text}");
+                }
+            }
+        }
+    }
+}
