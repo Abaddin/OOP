5c6e47234caa6b9c4e7262871bb13eb7dab27b37
Vlad Zorya <zorya.vladison@gmail.com>
2021-03-10 12:41:41 +0200

    Update functions and added MouseEventHandlers

--- Lab_2_New_Task2/Form1.cs
+++ Lab_2_New_Task2/Form1.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab_2_New_Task2
 {
-    public delegate void Opacity(double x);
-    
     public partial class Form1 : Form
     {
         public Form1()
@@ -55,6 +47,11 @@ namespace Lab_2_New_Task2
 
         private void button3_Click(object sender, EventArgs e)
         {
+            MessageBoxHello();
+        }
+
+        private void MessageBoxHello()
+        {
             MessageBox.Show(
                 "Hello World!", 
                 "Повідомлення", 
@@ -70,38 +67,57 @@ namespace Lab_2_New_Task2
                 MessageBoxButtons.OK, 
                 MessageBoxIcon.Information);
             
+        }
+        
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
             if (checkBox1.Checked)
             {
-                if (Opacity != 1)
-                {
-                    Opacity = 1;
-                }
-                else
-                {
-                    Opacity = 0.25;
-                }
+                button4.MouseClick += changeOpacity();
             }
-            
+            else
+            {
+                button4.MouseClick -= changeOpacity();
+            }
+        }
+
+        private MouseEventHandler changeOpacity()
+        {
+            return (o, args) => ChangeOpacity();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
             if (checkBox2.Checked)
             {
-                if (ActiveForm.BackColor == DefaultBackColor)
-                {
-                    ActiveForm.BackColor = Color.Chartreuse;
-                }
-                else
-                {
-                    ActiveForm.BackColor = DefaultBackColor;
-                }
+                button4.MouseClick += changeBackColor();
             }
-            
+            else
+            {
+                button4.MouseClick += changeBackColor();
+            }
+        }
+
+        private static MouseEventHandler changeBackColor()
+        {
+            return (o, args) => ChangeBGColor();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
             if (checkBox3.Checked)
             {
-                MessageBox.Show(
-                    "Hello World!", 
-                    "Повідомлення", 
-                    MessageBoxButtons.OK, 
-                    MessageBoxIcon.Information);
+                button4.MouseClick += messageBoxHello();
             }
+            else
+            {
+                button4.MouseClick -= messageBoxHello();
+            }
+        }
+
+        private MouseEventHandler messageBoxHello()
+        {
+            return (o, args) => MessageBoxHello();
         }
     }
 }
