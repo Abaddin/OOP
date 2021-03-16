7f49edebd233e43149bd83c49b6694681c25c5ff
Vlad Zorya <zorya.vladison@gmail.com>
2021-03-17 00:22:40 +0200

    Add individual task for Laboratory work 1

---
+++ Lab_1_New_Individual_v7/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Lab_1_New_Individual_v7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(312, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(415, 55);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(100, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(205, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(312, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(415, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 268);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        #endregion
+    }
+}
---
+++ Lab_1_New_Individual_v7/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1_New_Individual_v7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label1.Visible || label3.Visible)
+            {
+                if (label1.Visible && label3.Visible)
+                {
+                    label1.Visible = false;
+                    label3.Visible = false;
+                }
+                else if (label1.Visible)
+                {
+                    label1.Visible = false;
+                    label3.Visible = true;
+                }
+                else
+                {
+                    label1.Visible = true;
+                    label3.Visible = false;
+                }
+            }
+            else
+            {
+                label1.Visible = true;
+                label3.Visible = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (label1.Visible || label2.Visible)
+            {
+                if (label1.Visible && label2.Visible)
+                {
+                    label1.Visible = false;
+                    label2.Visible = false;
+                }
+                else if (label1.Visible)
+                {
+                    label1.Visible = false;
+                    label2.Visible = true;
+                }
+                else
+                {
+                    label1.Visible = true;
+                    label2.Visible = false;
+                }
+            }
+            else
+            {
+                label1.Visible = true;
+                label2.Visible = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (label2.Visible || label4.Visible)
+            {
+                if (label2.Visible && label4.Visible)
+                {
+                    label2.Visible = false;
+                    label4.Visible = false;
+                }
+                else if (label2.Visible)
+                {
+                    label2.Visible = false;
+                    label4.Visible = true;
+                }
+                else
+                {
+                    label2.Visible = true;
+                    label4.Visible = false;
+                }
+            }
+            else
+            {
+                label2.Visible = true;
+                label4.Visible = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (label2.Visible || label4.Visible)
+            {
+                if (label3.Visible && label4.Visible)
+                {
+                    label3.Visible = false;
+                    label4.Visible = false;
+                }
+                else if (label3.Visible)
+                {
+                    label3.Visible = false;
+                    label4.Visible = true;
+                }
+                else
+                {
+                    label3.Visible = true;
+                    label4.Visible = false;
+                }
+            }
+            else
+            {
+                label3.Visible = true;
+                label4.Visible = true;
+            }
+        }
+    }
+}
