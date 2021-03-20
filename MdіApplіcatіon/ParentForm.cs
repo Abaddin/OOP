06beba51102fa7f353434be89b483df6a140960b
Vlad Zorya <zorya.vladison@gmail.com>
2021-03-20 11:53:39 +0200

    Add task 3 for Laboratory work 3

---
+++ MdіApplіcatіon/ParentForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MdіApplіcatіon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    Form newChіld = new Form();
+                    newChіld.MdiParent = this;
+                    newChіld.Show();
+                    newChіld.Text = newChіld.Text + "";
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(MdiLayout.Cascade);
+                    break;
+                case "Tіtle":
+                    this.LayoutMdi(MdiLayout.TileHorizontal);
+                    break;
+            }
+        }
+    }
+}
