2d2768ed0c2b344b51f8a1e9dd8b935500620a2d
Vlad Zorya <zorya.vladison@gmail.com>
2021-03-24 10:17:07 +0200

    Added task 2 for Laboratory work 2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        delegate void Deledate(int sec);
+
+        public delegate void Metody();
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number seconds: ");
+            int sec = int.Parse(Console.ReadLine());
+            Timer timer = new Timer();
+            Deledate del = timer.Start;
+            del.Invoke(sec);
+        }
+    }
+}
---
+++ Task2/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using static Task2.Program;
+
+namespace Task2
+{
+    class Timer
+    {
+        private int seconds { get; set; }
+
+        public void Start(int sec)
+        {
+            Metody[] met = new Metody[] {Method1, Method2, Method3, Method4};
+            Random rand = new Random();
+            while (true)
+            {
+                Thread.Sleep(sec * 1000);
+                int a = rand.Next(0, 4);
+                met[a]();
+            }
+        }
+
+        private static void Method1()
+        {
+            Random rand = new Random();
+            int a = rand.Next(0, 100);
+            Console.WriteLine("Method 1. Random number: " + a);
+        }
+
+        private static void Method2()
+        {
+            Console.WriteLine("Method 2. Time: " + DateTime.Now.TimeOfDay);
+        }
+
+        private static void Method3()
+        {
+            Console.WriteLine("Method 3. Day of month: " + DateTime.Now.Day);
+        }
+        
+        private static void Method4()
+        {
+            Console.WriteLine("Method 4. Month: " + DateTime.Now.Month);
+        }
+    }
+}
